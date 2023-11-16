fd6e162ebd0901108dcd91cd58b48c6e1ff1b4d4
runas-genesis <ralom2023@elam.co.uk>
2023-11-16 15:41:09 +0000

    Enemies introduced!!!

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -32,6 +32,14 @@ public class PlayerMovement : MonoBehaviour
 
     }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy Head"))
+        {
+            Destroy(collision.transform.parent.gameObject);
+        }
+    }
+
     bool IsGrounded()
     {
         return Physics.CheckSphere(groundCheck.position, 0.1f, ground);
--- Assets/Scripts/WaypointFollower.cs
+++ Assets/Scripts/WaypointFollower.cs
@@ -11,6 +11,15 @@ public class WaypointFollower : MonoBehaviour
 
     void Update()
     {
+        if (Vector3.Distance(transform.position, waypoints[currentWaypointIndex].transform.position) < .1f)
+        {
+            currentWaypointIndex++;
+           if  (currentWaypointIndex >= waypoints.Length)
+            {
+                currentWaypointIndex = 0;
+            }
+        }
+        
         transform.position = Vector3.MoveTowards(transform.position, waypoints[currentWaypointIndex].transform.position, speed * Time.deltaTime);
     }
 }
